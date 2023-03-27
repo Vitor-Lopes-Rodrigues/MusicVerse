f4e1f70b1c904171cefcadbe19b2ddd54b80ecfe
vitor.rodrigues <vitor.rodrigues@cenpros.com.br>
2023-03-27 19:38:11 -0300

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        teste
+      </h1>
+    </div>
+  );
+}
+
+export default App;
