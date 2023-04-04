c7326a8ac6b140777eeeae2b7f09ef97bf0feb6a
Vitor-Lopes-Rodrigues <vitorlopes758@gmail.com>
2023-04-04 19:42:30 -0300

    Tela de cadastro padrao

--- front-end/src/pages/Cadastro/Cadastro.js
+++ front-end/src/pages/Cadastro/Cadastro.js
@@ -13,11 +13,10 @@ const Cadastro = () => {
     const [displayName, setDisplayName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
     const [error, setError] = useState("")
 
 
-    const { createUser, error: authError, loading } = useAuthentication()
+    // const { createUser, error: authError, loading } = useAuthentication()
 
 
     //Criando submit de formulário
@@ -31,19 +30,7 @@ const Cadastro = () => {
             email,
             password
         }
-
-        //Criando validação de usuario
-        if(password !== confirmPassword){
-            setError("As senhas precisam ser iguais")
-            return
-        }
-        console.log(user);
     }
-    useEffect(() =>{
-        if(authError){
-            setError(authError)
-        }
-    }, [authError])
     return(
         <div className={styles.register}>
             <h1>Cadastre-se para postar</h1>
@@ -61,16 +48,13 @@ const Cadastro = () => {
                     <span>Senha:</span>
                     <input type="password" name="password" required placeholder="Insira sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </label>
-                <label>
-                    <span>Confirmação de senha:</span>
-                    <input type="password" name="confirmPassword" required placeholder="Confirme a sua senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-                </label>
-                {!loading && <button className="btn">Cadastrar</button>}
-                {loading && (
-                    <button className="btn" disabled>
-                        Aguarde...
-                    </button>
-                )}
+                <button className="btn">Cadastrar</button>
+                {/*{!loading &&}*/}
+                {/*{loading && (*/}
+                {/*    <button className="btn" disabled>*/}
+                {/*        Aguarde...*/}
+                {/*    </button>*/}
+                {/*)}*/}
                 {error && <p className="error">{error}</p>}
             </form>
         </div>
