c1373e1e0c0e5b4af995ae38b65464127cb54b1d
vitor.rodrigues <vitor.rodrigues@cenpros.com.br>
2023-04-12 20:40:12 -0300

    Css do home com hooks e states iniciais

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -2,6 +2,10 @@
 import React from "react";
 //Importando css de "modules"
 
+//hooks
+import { useNavigation, Link} from "react-router-dom";
+import {useState} from "react";
+
 //Importando Components
 import HomeComponets from "../../components/HomeComponents/HomeComponets";
 
