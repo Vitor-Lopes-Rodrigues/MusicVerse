b32fe3aff26f21d099fb4547d2c5f8f1327d75ca
Vitor-Lopes-Rodrigues <vitorlopes758@gmail.com>
2023-05-29 10:38:42 -0300

    Criando poppup  de pagina de editar

--- front-end/src/pages/profile/index.js
+++ front-end/src/pages/profile/index.js
@@ -2,7 +2,10 @@ import React, {useEffect, useState} from "react";
 import Navbar from "../../components/navbar";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import { Modal } from 'react-bootstrap';
 import styles from "../../pages/profile/profile.module.css"
+
+
 
 const Profile = () => {
 
@@ -11,6 +14,38 @@ const Profile = () => {
     };
 
     const [user, setUser] = useState("");
+    const [showModal, setShowModal] = useState(false);
+    const [formValues, setFormValues] = useState({
+        username: '',
+        email: '',
+        phone: '',
+        city: '',
+        gender: '',
+        state: '',
+        country: ''
+    });
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Faça o que quiser com os valores do formulário
+        console.log(formValues);
+        setShowModal(false);
+    };
     const navigate = useNavigate()
 
     // Verificar se o usuario está logado
@@ -34,9 +69,9 @@ const Profile = () => {
     return(
         <>
             <Navbar/>
-            <div className={styles.profile}>
-            <img className={styles.profileImage} src={require("../../images/post/imagem_guitarra.png")} alt="Foto de perfil"/>
-                <div className={styles.profileInfo}>
+                <div className={styles.profile}>
+                    <img className={styles.profileImage} src={require("../../images/post/imagem_guitarra.jpg")} alt="Foto de perfil"/>
+                    <div className={styles.profileInfo}>
                         <div className={styles.profileName}>
                             <h1>{user.name} </h1>
                             <div className={styles.profileEmail}>
@@ -45,25 +80,109 @@ const Profile = () => {
                             <div className={styles.profilePhone}>
                                 <p>{user.phone}</p>
                             </div>
-                        <div className={styles.profileLocation}>
-                            <p>{user.city}</p>
+                            <div className={styles.profileLocation}>
+                                <p>{user.city}</p>
+                            </div>
+                            <div className={styles.profileGender}>
+                                <p>{user.gender}</p>
+                            </div>
+                            {/*<div className={styles.Password}>*/}
+                            {/*    <p>{user.password}</p>*/}
+                            {/*</div>*/}
+                            <div className={styles.profileStates}>
+                                <p>{user.state}</p>
+                            </div>
+                            <div className={styles.profileCounty}>
+                                <p>{user.country}</p>
+                            </div>
+                            <button onClick={handleClick} className="btn">Editar</button>
                         </div>
-                        <div className={styles.profileGender}>
-                            <p>{user.gender}</p>
-                        </div>
-                        {/*<div className={styles.Password}>*/}
-                        {/*    <p>{user.password}</p>*/}
-                        {/*</div>*/}
-                        <div className={styles.profileStates}>
-                            <p>{user.state}</p>
-                        </div>
-                        <div className={styles.profileCounty}>
-                            <p>{user.country}</p>
-                        </div>
-                            <button className="btn">Editar</button>
                     </div>
                 </div>
-            </div>
+                <div id="poppup-modal">
+                    <Modal show={showModal} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Editar Informações</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-group">
+                                    <label>Username:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="username"
+                                        value={formValues.username}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Email:</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        name="email"
+                                        value={formValues.email}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Phone:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="phone"
+                                        value={formValues.phone}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>City:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="city"
+                                        value={formValues.city}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Gender:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="gender"
+                                        value={formValues.gender}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>State:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="state"
+                                        value={formValues.state}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Country:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="country"
+                                        value={formValues.country}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <br/>
+                                <button type="submit" className="btn btn-primary">Salvar</button>
+                            <br/><br/>
+                            </form>
+                        </Modal.Body>
+                    </Modal>
+                </div>
         </>
     )
 }
